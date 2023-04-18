5f6dd74add97a1cc559f0a600f31d2c500a257aa
Oluwafemi Olusesi Ogundare <ogundarearnold@gmail.com>
2023-04-18 01:24:05 +0100

    String Interpolation

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,10 @@
+export default function getSanFranciscoDescription() {
+    const year = 2023;
+    const budget = {
+      income: '$119,868',
+      gdp: '$154.2 billion',
+      capita: '$178,479',
+    };
+  
+    return `As of ${year}, the average income in San Francisco is ${budget.income}, the GDP is ${budget.gdp}, and the capita is ${budget.capita}.`;
+  }
