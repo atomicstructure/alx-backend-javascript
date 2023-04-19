4c55bd4beb789b052d987fe155ea624b07d835ca
Oluwafemi Olusesi Ogundare <ogundarearnold@gmail.com>
2023-04-19 18:05:25 +0100

    Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const response = getResponseFromAPI();
-    console.log(response instanceof Promise);
-  });
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {});
 }
-
-getResponseFromAPI();
