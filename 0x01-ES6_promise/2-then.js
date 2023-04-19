407cfe16d58f2fc4d04f701fe69d6ab1319161be
Oluwafemi Olusesi Ogundare <ogundarearnold@gmail.com>
2023-04-19 19:48:34 +0100

    Catch me if you can

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then((response) => {
+    return { status: 200, body: 'success' };
+  })
+  .catch((error) => {return new Error();})
+  .finally(() => {console.log('Got a response from the API');});
+}
